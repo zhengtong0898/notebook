9f35ae961288f2e081a60a4d23cfd8d381ac1b46
zhengtong0898 <zhengtong0898@aliyun.com>
2021-03-18 14:40:19 +0800

    增加 Django.ModelAdmin.fieldsets 属性说明

--- django/admin-tutorial/AdminFields/AdminFields/settings.py
+++ django/admin-tutorial/AdminFields/AdminFields/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'exclude_.apps.ExcludeConfig',
     'fields_.apps.FieldsConfig',
+    'fieldsets_.apps.FieldsetsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
