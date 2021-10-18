fa84e0c4e895684041a819ff80582b5b0b8077a5
zhengtong0898 <zhengtong0898@aliyun.com>
2021-10-18 10:20:57 +0800

    algorithms: wz-course-week-1: essence-7

--- algorithms/wz-course/essence/matrix_col_to_row.py
+++ algorithms/wz-course/essence/matrix_col_to_row.py
@@ -1,39 +1,42 @@
-s = [
-    [1, 2, 3, 4, 5],
-    [1, 2, 3, 4, 5],
-    [1, 2, 3, 4, 5],
-    [1, 2, 3, 4, 5],
-    [1, 2, 3, 4, 5],
-]
-
-
-# 获取列: 列固定, 遍历行
-def get_column(index):
-    result = [[0] * len(i) for i in s]
-    for col in range(len(s)):                       # [col] 以列为主
-        for row in range(len(s)):                   # [row] 以行为辅
-            result[col][row] = s[row][col]          # [row][col] 内核逻辑: 列固定, 遍历行
-    return result[index]
+from typing import List
 
 
 # 列转行
-def swap_row_and_col():
-    ss = list(zip(*s))
+def swap_row_and_col_v1(matrix: List[List[int]]):
+    ss = list(zip(*matrix))
     return ss
 
 
-def main():
-    assert get_column(0) == [1, 1, 1, 1, 1]
-    assert get_column(1) == [2, 2, 2, 2, 2]
-    assert get_column(2) == [3, 3, 3, 3, 3]
-    assert get_column(3) == [4, 4, 4, 4, 4]
-    assert get_column(4) == [5, 5, 5, 5, 5]
+# 列转行
+def swap_row_and_col_v2(matrix: List[List[int]]):
+    result = [[0] * len(i) for i in matrix]
+    for col in range(len(matrix)):                       # [col] 以列为主
+        for row in range(len(matrix)):                   # [row] 以行为辅
+            result[col][row] = matrix[row][col]          # [row][col] 内核逻辑: 列固定, 遍历行
+    return result
 
-    assert swap_row_and_col() == [(1, 1, 1, 1, 1),
-                                  (2, 2, 2, 2, 2),
-                                  (3, 3, 3, 3, 3),
-                                  (4, 4, 4, 4, 4),
-                                  (5, 5, 5, 5, 5)]
+
+def main():
+    s = [
+        [1, 2, 3, 4, 5],
+        [1, 2, 3, 4, 5],
+        [1, 2, 3, 4, 5],
+        [1, 2, 3, 4, 5],
+        [1, 2, 3, 4, 5],
+    ]
+    result = swap_row_and_col_v1(s)
+    assert result == [(1, 1, 1, 1, 1),
+                      (2, 2, 2, 2, 2),
+                      (3, 3, 3, 3, 3),
+                      (4, 4, 4, 4, 4),
+                      (5, 5, 5, 5, 5)]
+
+    result = swap_row_and_col_v2(s)
+    assert result == [[1, 1, 1, 1, 1],
+                      [2, 2, 2, 2, 2],
+                      [3, 3, 3, 3, 3],
+                      [4, 4, 4, 4, 4],
+                      [5, 5, 5, 5, 5]]
 
 
 if __name__ == '__main__':
