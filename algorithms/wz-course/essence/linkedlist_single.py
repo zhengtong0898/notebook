500d3a73d5ede7d9122121373c33231177ef9652
zhengtong0898 <zhengtong0898@aliyun.com>
2021-10-28 21:16:55 +0800

    algorithms: wz-course-week-2: essence-4

--- algorithms/wz-course/essence/linkedlist_single.py
+++ algorithms/wz-course/essence/linkedlist_single.py
@@ -17,10 +17,10 @@ class Node:
         return self.value == other.value
 
 
-class LinkedListSingle:
+class LinkedListV1:
 
     """
-    单链表
+    链表(仅头节点)
 
     链表特征:
     入口是一个 head_node 对象, 末端 node 的 next 是 None.
@@ -101,7 +101,7 @@ class LinkedListSingle:
 
 
 def test_llv1_walk():
-    llv1 = LinkedListSingle()
+    llv1 = LinkedListV1()
     llv1.head.next = Node("a")
     llv1.head.next.next = Node("b")
     llv1.head.next.next.next = Node("c")
@@ -112,7 +112,7 @@ def test_llv1_walk():
 
 
 def test_llv1_find():
-    llv1 = LinkedListSingle()
+    llv1 = LinkedListV1()
     llv1.head.next = Node("a")
     llv1.head.next.next = Node("b")
     llv1.head.next.next.next = Node("e")
@@ -124,7 +124,7 @@ def test_llv1_find():
 
 
 def test_llv1_front_insert():
-    llv1 = LinkedListSingle()
+    llv1 = LinkedListV1()
     llv1.head.next = Node("a")
     llv1.head.next.next = Node("b")
     llv1.front_insert("c")
@@ -135,7 +135,7 @@ def test_llv1_front_insert():
 
 
 def test_llv1_back_insert():
-    llv1 = LinkedListSingle()
+    llv1 = LinkedListV1()
     llv1.head.next = Node("a")
     llv1.head.next.next = Node("b")
     llv1.back_insert("c")
@@ -146,7 +146,7 @@ def test_llv1_back_insert():
 
 
 def test_llv1_insert():
-    llv1 = LinkedListSingle()
+    llv1 = LinkedListV1()
     llv1.head.next = Node("a")
     llv1.head.next.next = Node("b")
     llv1.head.next.next.next = Node("c")
@@ -155,7 +155,7 @@ def test_llv1_insert():
 
 
 def test_llv1_front_delete():
-    llv1 = LinkedListSingle()
+    llv1 = LinkedListV1()
     llv1.head.next = Node("a")
     llv1.head.next.next = Node("b")
     llv1.head.next.next.next = Node("c")
@@ -168,7 +168,7 @@ def test_llv1_front_delete():
 
 
 def test_llv1_back_delete():
-    llv1 = LinkedListSingle()
+    llv1 = LinkedListV1()
     llv1.head.next = Node("a")
     llv1.head.next.next = Node("b")
     llv1.head.next.next.next = Node("c")
@@ -181,7 +181,7 @@ def test_llv1_back_delete():
 
 
 def test_llv1_delete():
-    llv1 = LinkedListSingle()
+    llv1 = LinkedListV1()
     llv1.head.next = Node("a")
     llv1.head.next.next = Node("b")
     llv1.head.next.next.next = Node("c")
@@ -202,8 +202,102 @@ def test_llv1():
     test_llv1_delete()
 
 
+class LinkedListV2(LinkedListV1):
+
+    """
+    链表(头尾节点)
+
+    链表特征:
+    双向链表由当前类对象来维护 虚拟头节点 和 虚拟尾节点，
+
+    备注:
+    增加尾节点, 可优化 back_insert 和 back_delete 的时间复杂度, 从 O(n) 至 O(1).
+    """
+
+    def __init__(self):
+        super(LinkedListV2, self).__init__()
+        self.tail = Node(value="tail", next_node=None)                  # 虚拟尾节点
+
+    def front_insert(self, value: str):
+        is_head_empty = not self.head.next
+        super(LinkedListV2, self).front_insert(value)
+        if is_head_empty:
+            self.tail = self.head.next
+
+    def insert(self, value: str, to_index: int):
+        index = 0
+        curr_node = self.head                                           # 从第0个节点(虚拟头节点)开始遍历,
+        while curr_node.next:                                           # 当遍历到列表最后一个元素时(没有越界), 停止遍历.
+            if to_index == index: break
+            curr_node = curr_node.next
+            index += 1
+
+        is_head_node = curr_node == self.head
+        is_tail_node = curr_node == self.tail
+        if is_head_node:
+            self.front_insert(value)
+        elif is_tail_node:
+            self.back_insert(value)
+        else:
+            new_node = Node(value)
+            new_node.next = curr_node.next
+            curr_node.next = new_node
+
+    def back_insert(self, value: str):
+        """
+        优化 back_insert, 从 O(n) 至 O(1).
+
+        链表内的结构不可以交给外部来创建，
+        这有可能会破坏链接内部的结构, 否则次优化无效.
+        """
+        new_node = Node(value=value)
+        is_head_empty = not self.head.next
+        if is_head_empty:
+            self.head.next = new_node
+            self.tail = self.head.next
+        else:
+            self.tail.next = new_node
+            self.tail = self.tail.next
+
+
+def test_llv2_front_insert():
+    lld = LinkedListV2()
+    lld.front_insert("a")
+    lld.front_insert("b")
+    lld.front_insert("c")
+    assert lld.walk() == ["c", "b", "a"]
+    assert lld.tail == Node("a")
+
+
+def test_llv2_insert():
+    lld = LinkedListV2()
+    lld.insert("a", 0)
+    lld.insert("b", 1)
+    lld.insert("c", 2)
+    lld.insert("d", 3)
+    lld.insert("e", 0)
+    assert lld.walk() == ["e", "a", "b", "c", "d"]
+    assert lld.tail == Node("d")
+
+
+def test_llv2_back_insert():
+    lld = LinkedListV2()
+    lld.back_insert("a")
+    lld.back_insert("b")
+    lld.back_insert("c")
+    assert lld.walk() == ["a", "b", "c"]
+    assert lld.tail == Node("c")
+
+
+def test_llv2():
+    test_llv2_front_insert()
+    test_llv2_insert()
+    test_llv2_back_insert()
+
+
 def main():
     test_llv1()
+    test_llv2()
 
 
 if __name__ == '__main__':
