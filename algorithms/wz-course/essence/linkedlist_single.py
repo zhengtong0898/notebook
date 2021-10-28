d193c3b2a42d1167961436697dee44294fc14960
zhengtong0898 <zhengtong0898@aliyun.com>
2021-10-28 13:40:23 +0800

    algorithms: wz-course-week-2: essence-3

--- algorithms/wz-course/essence/linkedlist_single.py
+++ algorithms/wz-course/essence/linkedlist_single.py
@@ -17,35 +17,35 @@ class Node:
         return self.value == other.value
 
 
-class LinkedListV1:
+class LinkedListSingle:
 
     """
     单链表
 
-    链表特征是入口是一个 head_node对象, 末端 node 的 next 是 None.
+    链表特征:
+    入口是一个 head_node 对象, 末端 node 的 next 是 None.
+
+    边界问题:
+    self.head       !=  None  ==  list[0]
+    self.head.next  !=  None  ==  list[1]
+    curr_node.next  ==  None  ==  list[-1]
+    curr_node       ==  None  ==  越界: list index out of range
     """
 
     def __init__(self):
         self.head = Node(value="head", next_node=None)                  # 虚拟头节点
 
     def walk(self) -> List[str]:
-        """
-        边界: 从 self.head.next 开始, 到 curr_node == None 结束; 等同于:
-             range(1, 10+1) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        """
         result = []
-        curr_node = self.head.next
-        while curr_node:
+        curr_node = self.head.next                                      # 第0个节点是虚拟头节点, 所以从第1个有效节点开始遍历,
+        while curr_node:                                                # 当遍历超出列表时(此时处于越界状态), 停止遍历.
             result.append(curr_node.value)
             curr_node = curr_node.next
         return result
 
     def find(self, value: str) -> Node:
-        """
-        range(1, 10+1) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        """
-        curr_node = self.head.next
-        while curr_node:
+        curr_node = self.head.next                                      # 第0个节点是虚拟头节点, 所以从第1个有效节点开始遍历,
+        while curr_node:                                                # 当遍历超出列表时(此时处于越界状态), 停止遍历.
             if curr_node.value == value: return curr_node
             curr_node = curr_node.next
         return curr_node.next
@@ -56,21 +56,15 @@ class LinkedListV1:
         self.head.next = new_node
 
     def back_insert(self, value: str):
-        """
-        range(0, 10) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        """
-        curr_node = self.head
-        while curr_node.next:
-            curr_node = curr_node.next
-        curr_node.next = Node(value)
+        curr_node = self.head                                           # 从第0个节点(虚拟头节点)开始遍历,
+        while curr_node.next:                                           # 当遍历到列表最后一个元素时(没有越界), 停止遍历.
+            curr_node = curr_node.next                                  # 之所以要从第0个节点开始, 是因为有可能是一个空链表,
+        curr_node.next = Node(value)                                    # 当链表为空时, 将新的节点写在第1个位置处.
 
     def insert(self, value: str, to_index: int):
-        """
-        range(0, 10) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        """
         index = 0
-        curr_node = self.head
-        while curr_node.next:
+        curr_node = self.head                                           # 从第0个节点(虚拟头节点)开始遍历,
+        while curr_node.next:                                           # 当遍历到列表最后一个元素时(没有越界), 停止遍历.
             if to_index == index: break
             curr_node = curr_node.next
             index += 1
@@ -81,39 +75,33 @@ class LinkedListV1:
     def front_delete(self):
         delete_node = self.head.next
         next_node = delete_node.next
-        self.head.next = next_node          # 解被引用, 即: 删除外部对象指向(引用)到 delete_node.
-        delete_node.next = None             # 解引用, 即: 删除 delete_node 对外部对象的指向(引用).
+        self.head.next = next_node                                      # 解被引用, 即: 删除外部对象指向(引用)到 delete_node.
+        delete_node.next = None                                         # 解引用, 即: 删除 delete_node 对外部对象的指向(引用).
 
     def back_delete(self):
-        """
-        range(0, 10+1) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        """
         prev_node = self.head
-        curr_node = self.head.next
-        while curr_node.next:
+        curr_node = self.head.next                                      # 从第0个节点(虚拟头节点)开始遍历,
+        while curr_node.next:                                           # 当遍历到列表最后一个元素时(没有越界), 停止遍历.
             prev_node = curr_node
             curr_node = curr_node.next
         prev_node.next = None
 
     def delete(self, to_index):
-        """
-        range(0, 10+1) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        """
         index = 0
         prev_node = self.head
-        curr_node = self.head.next
-        while curr_node.next:
+        curr_node = self.head.next                                      # 从第0个节点(虚拟头节点)开始遍历,
+        while curr_node.next:                                           # 当遍历到列表最后一个元素时(没有越界), 停止遍历.
             if to_index == index: break
             prev_node = curr_node
             curr_node = curr_node.next
 
         next_node = curr_node.next
-        prev_node.next = next_node          # 解被引用
-        curr_node.next = None               # 解引用
+        prev_node.next = next_node                                      # 解被引用
+        curr_node.next = None                                           # 解引用
 
 
 def test_llv1_walk():
-    llv1 = LinkedListV1()
+    llv1 = LinkedListSingle()
     llv1.head.next = Node("a")
     llv1.head.next.next = Node("b")
     llv1.head.next.next.next = Node("c")
@@ -124,7 +112,7 @@ def test_llv1_walk():
 
 
 def test_llv1_find():
-    llv1 = LinkedListV1()
+    llv1 = LinkedListSingle()
     llv1.head.next = Node("a")
     llv1.head.next.next = Node("b")
     llv1.head.next.next.next = Node("e")
@@ -136,7 +124,7 @@ def test_llv1_find():
 
 
 def test_llv1_front_insert():
-    llv1 = LinkedListV1()
+    llv1 = LinkedListSingle()
     llv1.head.next = Node("a")
     llv1.head.next.next = Node("b")
     llv1.front_insert("c")
@@ -147,7 +135,7 @@ def test_llv1_front_insert():
 
 
 def test_llv1_back_insert():
-    llv1 = LinkedListV1()
+    llv1 = LinkedListSingle()
     llv1.head.next = Node("a")
     llv1.head.next.next = Node("b")
     llv1.back_insert("c")
@@ -158,7 +146,7 @@ def test_llv1_back_insert():
 
 
 def test_llv1_insert():
-    llv1 = LinkedListV1()
+    llv1 = LinkedListSingle()
     llv1.head.next = Node("a")
     llv1.head.next.next = Node("b")
     llv1.head.next.next.next = Node("c")
@@ -167,7 +155,7 @@ def test_llv1_insert():
 
 
 def test_llv1_front_delete():
-    llv1 = LinkedListV1()
+    llv1 = LinkedListSingle()
     llv1.head.next = Node("a")
     llv1.head.next.next = Node("b")
     llv1.head.next.next.next = Node("c")
@@ -180,7 +168,7 @@ def test_llv1_front_delete():
 
 
 def test_llv1_back_delete():
-    llv1 = LinkedListV1()
+    llv1 = LinkedListSingle()
     llv1.head.next = Node("a")
     llv1.head.next.next = Node("b")
     llv1.head.next.next.next = Node("c")
@@ -193,7 +181,7 @@ def test_llv1_back_delete():
 
 
 def test_llv1_delete():
-    llv1 = LinkedListV1()
+    llv1 = LinkedListSingle()
     llv1.head.next = Node("a")
     llv1.head.next.next = Node("b")
     llv1.head.next.next.next = Node("c")
