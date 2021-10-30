306bac2e46d338cbbd7d67aaf379918172f205a2
zhengtong0898 <zhengtong0898@aliyun.com>
2021-10-30 22:35:33 +0800

    algorithms: wz-course-week-2: 0203

--- algorithms/wz-course/week-2/0203-remove-elements.py
+++ algorithms/wz-course/week-2/0203-remove-elements.py
@@ -28,6 +28,11 @@ class Solution:
     """
 
     def removeElements(self, head: ListNode, val: int) -> ListNode:
+        """
+        改变链表的万能写法
+
+        创建一个新的链表头节点, 将不等于 val 的节点, back_append 到新的链表头节点中.
+        """
         new_node = ListNode()
         tail_node = new_node
         curr_node = head
@@ -41,12 +46,17 @@ class Solution:
             next_node = curr_node.next              # curr_node 链条切换节点-1: 先将下一个节点取出来
             curr_node.next = None                   # curr_node 链条解引用, 将链条截断后变成单一的节点.
             tail_node.next = curr_node              # tail_node 链条 back_append 尾部添加 curr_node 这个单一节点.
-            tail_node = curr_node                   # tail_node 链条切换到下一个节点.
+            tail_node = curr_node                   # tail_node 链条切换到下一个节点(时刻都要处于链表的尾部).
             curr_node = next_node                   # curr_node 链条切换节点-2: 切换到下一个节点
 
         return new_node.next
 
     def removeElements_v2(self, head: ListNode, val: int) -> ListNode:
+        """
+        遍历链表, 将等于 val 的节点链接解除, 并重新链接到下一个节点.
+
+        这段代码并不能通过 leet-code 的测试.
+        """
         prev_node = ListNode(next_=head)
         curr_node = head
         while curr_node:
