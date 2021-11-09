44f4b82d583a4493b0ac2acff4bd93960b2e4fc3
zhengtong0898 <zhengtong0898@aliyun.com>
2021-11-10 07:55:14 +0800

    bigdata: add comment

--- bigdata/mapreduce-py/MapReduce-Python.md
+++ bigdata/mapreduce-py/MapReduce-Python.md
@@ -1,6 +1,6 @@
 ### 按书上的操作
 ```shell
-$ cat sample.txt | python3.6 max_temperature_map.py | python3.6 max_temperature_reduce.py 
+$ cat sample.txt | python3 max_temperature_map.py | python3 max_temperature_reduce.py 
 1950    22
 1949    111
 ```
--- bigdata/mapreduce-py/max_temperature_reduce.py
+++ bigdata/mapreduce-py/max_temperature_reduce.py
@@ -2,19 +2,29 @@ import sys
 
 
 last_key = None
-max_val = -sys.maxsize
+max_val = 0
 
 
 for line in sys.stdin.readlines():
     key, val = line.strip().split("\t")
 
-    if last_key and last_key != key:
-        print("%s\t%s" % (last_key, max_val))
+    # init, set max_val
+    if not last_key:
         last_key = key
         max_val = int(val)
-    else:
+        continue
+
+    # same key, choose max val
+    if last_key == key:
         last_key = key
         max_val = max(max_val, int(val))
+        continue
+
+    # diff key, set max_val
+    print("%s\t%s" % (last_key, max_val))
+    last_key = key
+    max_val = int(val)
+
 
 if last_key:
     print("%s\t%s" % (last_key, max_val))
