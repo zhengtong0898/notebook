a24e5cb3851f1dc86aff13cf2414e2a53ad16348
zhengtong0898 <zhengtong0898@aliyun.com>
2022-02-27 23:48:43 +0800

    algorithms: merge sort

--- algorithms/sorts/ComparisonSorting/merge_sort.py
+++ algorithms/sorts/ComparisonSorting/merge_sort.py
@@ -11,43 +11,54 @@ def merge_sort(items):
     if len(items) <= 1:
         return None
 
-    # Finding the mid of the array
+    # 对半除数
+    # 左侧数量要么 == 右侧数量
+    # 左侧数量要么 == 右侧数量 - 1
     mid = len(items) // 2
 
-    # Dividing the array elements
+    # 切割成 left 和 right 两个列表
     left = items[:mid]
-
-    # into 2 halves
     right = items[mid:]
 
-    # Sorting the first half
+    # 递归
+    #
+    # 情况-1: 初期阶段 -> 层层递进
+    # 持续对半除，直到 len(left) == 1, len(right) == 1, 停止递进, 进入下面的 while 代码.
+    #
+    # 情况-2: 中期阶段 -> 层层回归
+    # len(left) 可能会 > 1 , 隐藏条件: 此时 left 列表时一个有序列表.
+    # len(right) 也可能会 > 1 , 隐藏条件: 此时 left 列表时一个有序列表.
+    # 对两个有序列表的比较, 就是用两个游标分别标记在两个列表的第0个位置,
+    # 比较后将小的提取出来, 然后小的那个列表游标右移一位.
     merge_sort(left)
-
-    # Sorting the second half
     merge_sort(right)
 
-    i = j = k = 0
+    l = r = i = 0                               # l是left的游标; r是right的游标; i是items的游标;
 
-    # Copy data to temp arrays L[] and R[]
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            items[k] = left[i]
-            i += 1
-        else:
-            items[k] = right[j]
-            j += 1
-        k += 1
+    # items 是两个列表总合的长度;
+    # items 会被接下来的代码重写称为一个有序列表;
+    while l < len(left) and r < len(right):     # 当 游标l 和 游标r 都没有越界时, 进入循环.
+        if left[l] < right[r]:                  # 当 左侧元素 小于 右侧元素 时,
+            items[i] = left[l]                  # 将小的元素(左侧元素)写入到items[i]的位置, 即items是一个有序列表.
+            l += 1                              # 游标l 右移一步(也就是游标i递增1), 为下一次比较做准备.
+        else:                                   # 当 左侧元素 大于 右侧元素 时,
+            items[i] = right[r]                 # 将小的元素(右侧元素)写入到items[i]的位置, 即items是一个有序列表.
+            r += 1                              # 游标j 右移一步(也就是游标j递增1), 为下一次比较做准备.
+        i += 1                                  # 游标i递增1, 为下一次写入做准备.
 
-    # Checking if any element was left
-    while i < len(left):
-        items[k] = left[i]
+    # 游标还没有右移到 left 列表的尽头,
+    # 将后续所有元素都写入到 items 末端.
+    while l < len(left):
+        items[i] = left[l]
+        l += 1
         i += 1
-        k += 1
 
-    while j < len(right):
-        items[k] = right[j]
-        j += 1
-        k += 1
+    # 游标还没有右移到 right 列表的尽头,
+    # 将后续所有元素写入到 items 末端.
+    while r < len(right):
+        items[i] = right[r]
+        r += 1
+        i += 1
 
 
 if __name__ == '__main__':
