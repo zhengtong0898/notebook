33794ce1a8e6711a9a6b0465976e44630241059e
zhengtong0898 <zhengtong0898@aliyun.com>
2022-02-28 22:30:44 +0800

    algorithms: quick sort partition 2

--- algorithms/sorts/ComparisonSorting/quick_sort_partition.py
+++ algorithms/sorts/ComparisonSorting/quick_sort_partition.py
@@ -1,8 +1,7 @@
-# 快速排序-
+# 快速排序-分割版本
 # 原理: https://www.bilibili.com/video/BV1at411T75o?from=search&seid=16200417356150128649
 # 参考: https://github.com/TheAlgorithms/Python/blob/master/sorts/quick_sort.py
 # 时间复杂度: Average-O(n*log n)    Worst-O(n2)    Best-O(n*log n)    Space-O(log n)    Stability-No
-import copy
 import random
 
 
@@ -10,13 +9,11 @@ import random
 # start and end points to first and last element of
 # an array respectively
 def partition(start, end, array):
-    array_copied = copy.deepcopy(array)
-
     # why: 这里为什么要 pivot_index = start         而不是直接  pivot_index = 0 ?
     # ans: 因为 partition 函数不知道外部传入进来的start是什么, 所以不能直接写 0 .
     #
     # why: 这里为什么要 pivot = array[pivot_index] 而不是直接  pivot = array[start] ?
-    # ans: 因为 start 是一个游标, 它的值会发生变化, 所以在在它发生变化之前, 使用 pivot_index 来记录它初始的状态.
+    # ans: 因为 start 是一个游标, 它的值会发生变化, 所以在它发生变化之前, 使用 pivot_index 来记录它初始的状态.
     pivot_index = start
     pivot = array[pivot_index]
 
@@ -45,6 +42,7 @@ def partition(start, end, array):
 
     # 将 end游标所在位置的值 与 pivot_index所在位置的值 交换位置.
     # 目的: 交换位置后, end位置左侧的所有元素都小于 pivot, end位置右侧的所有元素都大于 pivot.
+    # 重点: pivot所在的位置就是一个排好序的位置, 每次循环其实有效的排序就是pivot.
     array[end], array[pivot_index] = array[pivot_index], array[end]
 
     # 返回 end 游标, 此时 end 就是一个很分割点, 所以这个函数叫做 partition.
@@ -64,7 +62,6 @@ def quick_sort(start, end, array):
 
 if __name__ == '__main__':
     collection = random.sample(range(0, 20), 10)
-    collection_copied = copy.deepcopy(collection)
     sorted_collection = sorted(collection)
     quick_sort(0, len(collection) - 1, collection)
     assert collection == sorted_collection
