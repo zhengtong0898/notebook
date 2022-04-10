d1d3388e68a9bfc9221b2aeaedf873736cbfa171
zhengtong0898 <zhengtong0898@aliyun.com>
2022-04-10 11:41:31 +0800

    pluggy: 增加<importlib_metadata.distributions 里面是什么?>介绍

--- tester/pytest/docs/pluggy/testing/test_importlib_distribution.py
+++ tester/pytest/docs/pluggy/testing/test_importlib_distribution.py
@@ -1,33 +1,3 @@
-### DistFacade 类对象的介绍  
-
-这是一个`插件元数据`代理对象, 利用`__dir__`和`__getattr__`代理访问`self._dist`对象.  
-注: `importlib_metadata.Distribution` 对象存储的是单个已安装的`setuptools`包, 可通过`pip list`查询出来的包.  
-
-```python3
-
-class DistFacade:
-    """Emulate a pkg_resources Distribution"""
-
-    def __init__(self, dist: importlib_metadata.Distribution) -> None:
-        self._dist = dist
-
-    @property
-    def project_name(self) -> str:
-        name: str = self.metadata["name"]
-        return name
-
-    def __getattr__(self, attr: str, default=None):
-        return getattr(self._dist, attr, default)
-
-    def __dir__(self) -> List[str]:
-        return sorted(dir(self._dist) + ["_dist", "project_name"])
-
-```
-
-
-&nbsp;  
-### importlib_metadata.distributions 里面是什么?  
-```python3
 import sys
 
 
@@ -122,4 +92,3 @@ name: eggsample; value: eggsample.host:main; group: console_scripts
 
 ============================== 1 passed in 0.04s ===============================
 """
-```
